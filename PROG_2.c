74462b2bb6b6de439b9b17a831c3be5e8c297787
DevS0ni <88739819+DevS0ni@users.noreply.github.com>
2021-08-14 22:00:43 +0530

    Add files via upload

---
+++ PROG_2.c
@@ -0,0 +1,10 @@
+/* [2.] Write a C program to print Hello , 
+How are you and Good morning in different lines.*/
+#include<stdio.h>
+int main()
+{
+    printf("Hello !!");
+    printf("\nHow are you ?!");
+    printf("\nGood Morning");
+	return 0;
+}
