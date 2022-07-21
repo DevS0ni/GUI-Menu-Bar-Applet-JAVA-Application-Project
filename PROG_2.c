0e6d577a9eb2e580f51e19a04178d7027e1d2731
Dev Soni <88739819+DevS0ni@users.noreply.github.com>
2022-07-20 23:17:30 -0400

    Update PROG_2.c

--- PROG_2.c
+++ PROG_2.c
@@ -1,6 +1,6 @@
 /* [2.] Write a C program to print Hello , 
 How are you and Good morning in different lines.*/
-#include<stdio.h>
+#include<stdio.h>  // Declaring the header file standard input output in the C program.
 int main()
 {
     printf("Hello !!");
