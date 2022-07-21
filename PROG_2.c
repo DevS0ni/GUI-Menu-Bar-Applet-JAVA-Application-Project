ab1820c6964bbbe5178c70bfe0f6996a59edba89
Dev Soni <88739819+DevS0ni@users.noreply.github.com>
2022-07-20 23:19:48 -0400

    Update PROG_2.c

--- PROG_2.c
+++ PROG_2.c
@@ -5,6 +5,6 @@ int main()  // Declaration of the Main or Startup function in the C program.
 {
     printf("Hello !!");  // Printing the message "Hello !!" to the console screen.
     printf("\nHow are you ?!");  // Printing the message "How are you ?!" on the console screen.
-    printf("\nGood Morning");
-	return 0;
+    printf("\nGood Morning");  // Printing the message "Good Morning" onto the console screen.
+    return 0;  // 
 }
