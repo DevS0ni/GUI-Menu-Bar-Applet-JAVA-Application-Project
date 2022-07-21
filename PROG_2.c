ef26a83261a3e8014283d9066775795413e7a921
Dev Soni <88739819+DevS0ni@users.noreply.github.com>
2022-07-20 23:18:32 -0400

    Update PROG_2.c

--- PROG_2.c
+++ PROG_2.c
@@ -3,7 +3,7 @@ How are you and Good morning in different lines.*/
 #include<stdio.h>  // Declaring the header file standard input output in the C program.
 int main()  // Declaration of the Main or Startup function in the C program.
 {
-    printf("Hello !!");
+    printf("Hello !!");  // Printing the message "Hello !!" to the console screen.
     printf("\nHow are you ?!");
     printf("\nGood Morning");
 	return 0;
